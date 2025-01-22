5c7def860c3e82ece9af3e69e49c8679be1f4ca2
wilsonh0 <E0959315@u.nus.edu>
2025-01-22 23:33:44 +0800

    Add support for echo

--- src/main/java/Oogway.java
+++ src/main/java/Oogway.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Oogway {
     private static final String name = "Master Oogway";
 
@@ -6,22 +8,39 @@ public class Oogway {
     }
 
     public static void introductionMessage() {
+        horizontalLine();
         System.out.println("Greetings, young one. I am " + name + ".\nEnlighten me... What do you seek?");
+        horizontalLine();
     }
 
     public static void exitMessage() {
+        horizontalLine();
         System.out.println("Farewell, young one. I hope to guide you again someday.");
         System.out.println("And never forget... Yesterday is history, Tomorrow is a mystery, but Today is a gift. That is why it is called the present.");
+        horizontalLine();
     }
 
     public static void main(String[] args) {
         // Introduction
-        horizontalLine();
         introductionMessage();
-        horizontalLine();
+
+        // Read user input
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String msg = sc.nextLine();
+
+            if (msg.equals("bye")) {
+                break;
+            }
+
+            // Echo
+            horizontalLine();
+            System.out.println(msg);
+            horizontalLine();
+        }
 
         // Exit
         exitMessage();
-        horizontalLine();
     }
 }
