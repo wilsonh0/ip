1ce2f6b9337aa47547df029740743800f7110ff3
wilsonh0 <E0959315@u.nus.edu>
2025-01-24 02:43:07 +0800

    feat: Add support for tracking ToDo, Event, Deadline tasks
    refactor: Replaced horizontalLine() with wrapMessage() for flexibility

--- src/main/java/Oogway.java
+++ src/main/java/Oogway.java
@@ -5,66 +5,85 @@ public class Oogway {
     private static final String NAME = "Master Oogway";
     private static final ArrayList<Task> tasks = new ArrayList<>();
 
+    private static final String LINE = "____________________________________________________________";
+
     /**
-     * Prints a horizontal line.
+     * Wraps message with horizontal lines.
      */
-    private static void horizontalLine() {
-        System.out.println("____________________________________________________________");
+    private static void wrapMessage(String message) {
+        System.out.printf("%s\n%s\n%s%n", Oogway.LINE, message, Oogway.LINE);
     }
 
     /**
      * Prints the introduction message.
      */
     private static void introductionMessage() {
-        horizontalLine();
-        System.out.println("Greetings, young one. I am " + NAME + ".");
-        System.out.println("Enlighten me... What do you seek?");
-        horizontalLine();
+        String message = "Greetings, young one. I am " + NAME + ".\n Enlighten me... What do you seek?";
+
+        wrapMessage(message);
     }
 
     /**
      * Prints the exit message.
      */
     private static void exitMessage() {
-        horizontalLine();
-        System.out.println("Farewell, young one. I hope to guide you again someday.");
-        System.out.println("And never forget... Yesterday is history, Tomorrow is a mystery, but Today is a gift. That is why it is called the present.");
-        horizontalLine();
+        String message = "Farewell, young one. I hope to guide you again someday.\n"
+                + "And never forget... Yesterday is history, Tomorrow is a mystery, but Today is a gift. "
+                + "That is why it is called the present.";
+
+        wrapMessage(message);
     }
 
     /**
      * Adds a task to the task list.
-     * @param description the input string containing the task description
+     * @param userInput the input string containing the task description
      */
-    private static void addTask(String description) {
+    private static void addTask(String taskType, String userInput) {
         // Checks if string is empty
-        if (description == null || description.trim().isEmpty()) {
-            horizontalLine();
-            System.out.println("Ah, young one, tasks cannot be empty.");
-            horizontalLine();
+        if (userInput == null || userInput.trim().isEmpty()) {
+            wrapMessage("Ah, young one, tasks cannot be empty.");
             return;
         }
-        tasks.add(new Task(description));
 
-        horizontalLine();
-        System.out.println("Added: " + description);
-        horizontalLine();
+        Task task = null;
+        String taskDescription = userInput.split(" ", 2)[1];
+
+        switch (taskType) {
+            case "todo" -> task = new ToDo(taskDescription);
+            case "deadline" -> {
+                String[] splitBySlash = taskDescription.split(" /by ", 2);
+                task = new Deadline(splitBySlash[0], splitBySlash[1]);
+
+            }
+            case "event" -> {
+                String[] splitBySlash = taskDescription.split(" /from | /to ", 3);
+                task = new Event(splitBySlash[0], splitBySlash[1], splitBySlash[2]);
+            }
+        }
+
+        if (task != null) {
+            tasks.add(task);
+
+            String message = "Alright, I have noted down the task:\n  "
+                    + task + "\nNow you have " + tasks.size() + " tasks in the list.";
+
+            wrapMessage(message);
+        }
     }
 
     /**
      * Lists all tasks in the task list.
      */
     private static void listTasks() {
-        horizontalLine();
         if (tasks.isEmpty()) {
-            System.out.println("Ah, it seems you have no tasks yet, young one.");
+            wrapMessage("Ah, it seems you have no tasks yet, young one.");
         } else {
-            System.out.println("Here are the tasks in your list:");
+            StringBuilder message = new StringBuilder("Here are the tasks in your list:\n");
             for (int i = 0; i < tasks.size(); i++) {
-                System.out.println((i + 1) + "." + tasks.get(i));
+                message.append(((i + 1))).append(".").append(tasks.get(i)).append("\n");
             }
+            wrapMessage(String.valueOf(message.toString()));
         }
-        horizontalLine();
     }
 
     /**
@@ -73,18 +92,20 @@ public class Oogway {
      */
     private static void handleMark(String userInput) {
         String[] arr = userInput.split(" ");
-        horizontalLine();
+        String message;
+
         try {
             int index = Integer.parseInt(arr[1]) - 1; // Convert to zero-based index
             tasks.get(index).setDone();
-            System.out.println("Ah, young one, it brings me great joy to see progress. I have marked this task as complete for you:");
-            System.out.println("  " + tasks.get(index));
+            message = "Ah, young one, it brings me great joy to see progress. I have marked this task as complete for you:\n";
+            message += "  " + tasks.get(index);
         } catch (NumberFormatException e) {
-            System.out.println("Tis not a valid number...");
+            message = "Tis not a valid number...";
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("Ah, young one, that task does not exist.");
+            message = "Ah, young one, that task does not exist.";
         }
-        horizontalLine();
+
+        wrapMessage(message);
     }
 
     /**
@@ -93,18 +114,20 @@ public class Oogway {
      */
     private static void handleUnmark(String userInput) {
         String[] arr = userInput.split(" ");
-        horizontalLine();
+        String message;
+
         try {
             int index = Integer.parseInt(arr[1]) - 1; // Convert to zero-based index
             tasks.get(index).setUndone();
-            System.out.println("Patience, young one. I have returned this task to its unfinished state:");
-            System.out.println("  " + tasks.get(index));
+            message = "Patience, young one. I have returned this task to its unfinished state:\n";
+            message += "  " + tasks.get(index);
         } catch (NumberFormatException e) {
-            System.out.println("Tis not a valid number...");
+            message = "Tis not a valid number...";
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("Ah, young one, that task does not exist.");
+            message = "Ah, young one, that task does not exist.";
         }
-        horizontalLine();
+
+        wrapMessage(message);
     }
 
     public static void main(String[] args) {
@@ -114,19 +137,26 @@ public class Oogway {
         // Read user input
         Scanner sc = new Scanner(System.in);
 
+        label:
         while (sc.hasNextLine()) {
             String userInput = sc.nextLine();
+            String firstWord = userInput.split(" ")[0];
 
-            if (userInput.equals("bye")) {
-                break;
-            } else if (userInput.equals("list")) {
-                listTasks();
-            } else if (userInput.startsWith("mark ")) {
-                handleMark(userInput);
-            } else if (userInput.startsWith("unmark ")) {
-                handleUnmark(userInput);
-            } else {
-                addTask(userInput);
+            switch (firstWord) {
+                case "bye":
+                    break label;
+                case "list":
+                    listTasks();
+                    break;
+                case "mark":
+                    handleMark(userInput);
+                    break;
+                case "unmark":
+                    handleUnmark(userInput);
+                    break;
+                default:
+                    addTask(firstWord, userInput);
+                    break;
             }
         }
 
